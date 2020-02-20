949a32db831d3eea5072a9a743cc076e4f54929c
nour <nour7anelsto7y@gmail.com>
2020-02-20 22:07:07 +0200

    fix error in category model

--- category/models.py
+++ category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Categories(models.model):
+class Categories(models.Model):
     cat_id = models.AutoField(primary_key=True)
     name = name = models.CharField(max_length=200)
