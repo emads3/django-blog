4e8b4ee23f995380284b4b04c96e12d43941dc8b
nour <nour7anelsto7y@gmail.com>
2020-02-25 22:09:45 +0200

    add register and accounts urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 from authentication import views
+
 urlpatterns = [
-    path('home', views.home)
+    path('home/', views.home, name="home"),
+    path('reg/', views.register, name="reg"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
