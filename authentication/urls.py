46b706817d14efaf5a8a95c10a8125467fda085f
nour <nour7anelsto7y@gmail.com>
2020-02-25 22:13:01 +0200

    modify urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from authentication import views
 
 urlpatterns = [
     path('home/', views.home, name="home"),
-    path('reg/', views.register, name="reg"),
+    path('register/', views.register, name="register"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
