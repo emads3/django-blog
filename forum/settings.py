35139dc5a8b4617dbd184af22fa4dbed0041a727
nour <nour7anelsto7y@gmail.com>
2020-02-25 22:11:08 +0200

    add templates, redirect login, redirect logout, and static path to setting.py

--- forum/settings.py
+++ forum/settings.py
@@ -57,7 +57,11 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join('authentication/templates'),
+            os.path.join('categories/templates')
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL =  '/static/'
+STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
