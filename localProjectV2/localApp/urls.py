ead1f88a24cfd09c98597bdb33a93cd92b748292
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-02-26 15:51:41 +0200

    paths are working now

--- localProjectV2/localApp/urls.py
+++ localProjectV2/localApp/urls.py
@@ -3,11 +3,11 @@ from localApp import views
 
 urlpatterns = [
 	path('home/', views.home),
-	path('addpost/',views.addpost),
-	path('showpost/<num>/',views.details),
-	path('sportsCat/',views.sportsCat),
-	path('foodCat/',views.foodCat),
-	path('booksCat/',views.booksCat),
+	path('addpost/',views.addpost,name='addpost'),
+	path('showpost/<num>/',views.details,name='showpost'),
+	path('sportsCat/',views.sportsCat,name='sportsCat'),
+	path('foodCat/',views.foodCat,name='foodCat'),
+	path('booksCat/',views.booksCat,name='booksCat'),
 	path('likes/<num>/',views.likes),
-	path('adduser/',views.adduser)
+	path('adduser/',views.adduser,name='adduser')
 ]
--- localProjectV2/localApp/views.py
+++ localProjectV2/localApp/views.py
@@ -8,13 +8,13 @@ def home(request):
 	context = {'latest_post_list' : latest_post_list}
 	return render(request,'localApp/home.html',context)
 
-def details(request,num):
+def details(request,num):		#for when a user wants to see the details of a specific post
 	post = Posts.objects.get(id = num)
 	context = { 'post_obj':post }
 	return render(request,'localApp/details.html',context)
 
 
-def addpost(request):
+def addpost(request):			#if a user wants to add a post
 	if request.method == 'POST':
 		post_form = PostForm(request.POST)
 		if post_form.is_valid():
@@ -25,29 +25,30 @@ def addpost(request):
 		context = { 'post_form':post_form }
 		return render(request , 'localApp/post_add.html' , context)
 
-def sportsCat(request):
+def sportsCat(request):			#if the sports category button was pressed
 	sports_cat = Posts.objects.filter(cat = 1)
 	context = { 'sports_cat':sports_cat }
 	return render(request , 'localApp/sports_cat.html' , context)
 
-def foodCat(request):
+def foodCat(request):			#if the food category button was pressed
 	food_cat = Posts.objects.filter(cat = 2)
 	context = { 'food_cat':food_cat }
 	return render(request , 'localApp/food_cat.html' , context)
 
-def booksCat(request):
+def booksCat(request):			#if the food category button was pressed
 	books_cat = Posts.objects.filter(cat = 3)
 	context = { 'books_cat':books_cat }
 	return render(request , 'localApp/books_cat.html' , context)
 
-def likes(request,num):
+def likes(request,num):			#if the like button was pressed
 	post = Post_Likes_Dislikes.objects.get(post = num)
 	post.likes += 1
 	post_likes = post.save()
 	context = { 'post_likes':post_likes }
 	return render(request , '/localApp/home.html' ,context)
 
-def adduser(request):
+def adduser(request):			#if the register button was pressed
+	user_form = UserForm()		
 	if request.method == 'POST':
 		user_form = UserForm(request.POST)
 		if user_form.is_valid():
@@ -57,3 +58,6 @@ def adduser(request):
 		user_form = UserForm()
 		context = { 'user_form':user_form }
 		return render(request , 'localApp/user_add.html' , context)
+
+# def comments(request,num):
+# 	comment = Comments.objects.filter(post_id = num)
