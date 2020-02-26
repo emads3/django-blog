163fe97cd0818f7a6a9c47876538b0ac64b42f1e
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-02-26 21:40:51 +0200

    added all the tables of the database

--- djangoApp/admin.py
+++ djangoApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Posts,Comments,Users,Post_Likes_Dislikes,Categories
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(Posts)
+admin.site.register(Comments)
+admin.site.register(Post_Likes_Dislikes)
+admin.site.register(Categories)
